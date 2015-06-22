d2bcc881580b8223bf2be89f8f6930a6f056c637
Alain Beauvois <alain@itipic.com>
2015-06-22 20:44:59 +0000

    Add docker remote redis support

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
-/*exports.database = {
-	type: 'mongodb',
-	hostname: 'localhost',
-	port: 27017,
-	database: 'scrumblr'
-};
-*/
-
 exports.database = {
-	type: 'redis',
-	prefix: '#scrumblr#'
+    type: 'redis',
+    hostname:  (process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.0.1' ),
+    port: (process.env.REDIS_PORT_6379_TCP_PORT || 6379),
+    prefix: '#scrumblr#'
 };
-
